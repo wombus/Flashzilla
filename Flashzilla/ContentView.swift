6cccc2444737c45e9e5c4678a49dcf257e52b739
wombus <peter.napoli@gmail.com>
2023-02-02 21:00:45 -0500

    update

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -1,41 +1,73 @@
 import SwiftUI
 
-struct ContentView: View {
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
 
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
+
+
+struct ContentView: View {
+    
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+    
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+    }
+    
+    
+    
     
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                            .stacked(at: index, in: cards.count)
+                    }
                 }
-                
             }
-        
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-                
+        }
+        .onReceive(timer) { time in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
             }
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                isActive = true
+            } else {
+                isActive = false
+            }
+        }
+        }
+
+        }
         
-        let combined = pressGesture.sequenced(before: dragGesture)
         
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
-        
-    }
-        
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
